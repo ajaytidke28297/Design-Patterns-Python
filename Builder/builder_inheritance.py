ab21ae9afe7f690509c5622ceb33e9590dc5f437
ajaytidke28297 <ajaytidke28297@gmail.com>
2022-03-10 23:05:45 +0530

    Add builder inheritance that solves the problem of previous one

---
+++ Builder/builder_inheritance.py
@@ -0,0 +1,54 @@
+class Person:
+
+    def __init__(self):
+        self.name = None
+        self.position = None
+        self.date_of_birth = None
+
+    def __str__(self):
+        return f"""{self.name} born on {self.date_of_birth} works as {self.position}"""
+
+    @staticmethod
+    def new():
+        return PersonBuilder()
+
+
+class PersonBuilder:
+
+    def __init__(self):
+        self.person = Person()
+
+    def build(self):
+        return self.person
+
+
+class PersonInfoBuilder(PersonBuilder):
+
+    def called(self, name):
+        self.person.name = name
+        return self
+
+
+class PersonJobBuilder(PersonInfoBuilder):
+
+    def works_as_a(self, position):
+        self.person.position = position
+        return self
+
+
+class PersonBirthDateBuilder(PersonJobBuilder):
+
+    def born(self, date_of_birth):
+        self.person.date_of_birth = date_of_birth
+        return self
+
+
+person_builder = PersonBirthDateBuilder()
+new_person = person_builder \
+    .called('Ajay') \
+    .works_as_a('Software Engineer') \
+    .born('28/2/1997') \
+    .build()
+
+print(new_person)
+
